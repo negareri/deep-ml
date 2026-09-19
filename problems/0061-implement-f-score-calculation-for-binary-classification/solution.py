0a4ec60f8a1965af209cc64542f20ba35cc56cba
negareri <114356079+negareri@users.noreply.github.com>
2026-09-19 12:48:37 +0000

    Update: Implement F-Score Calculation for Binary Classification

--- problems/0061-implement-f-score-calculation-for-binary-classification/solution.py
+++ problems/0061-implement-f-score-calculation-for-binary-classification/solution.py
@@ -1,40 +1,40 @@
 import numpy as np
 
 def f_score(y_true, y_pred, beta):
-	"""
-	Calculate F-Score for a binary classification task.
+    """
+    Calculate F-Score for a binary classification task.
 
-	:param y_true: Numpy array of true labels
-	:param y_pred: Numpy array of predicted labels
-	:param beta: The weight of precision in the harmonic mean
-	:return: F-Score rounded to three decimal places
-	"""
-	
-	TP = 0
-	FP = 0
-	FN = 0
+    :param y_true: Numpy array of true labels
+    :param y_pred: Numpy array of predicted labels
+    :param beta: The weight of precision in the harmonic mean
+    :return: F-Score rounded to three decimal places
+    """
 
-	for i in range(len(y_true)):
-		if y_true[i] == 1 and y_pred[i] == 1:
-			TP += 1
-		elif y_true[i] == 0 and y_pred[i] ==1:
-			FP += 1
-		elif y_true[i] == 1 and y_pred[i] == 0:
-			FN += 1
-	
-	if TP + FP == 0:
-		precision = 0.0
-	else:
-		precision = TP / (TP+FP)
+    TP = 0
+    FP = 0
+    FN = 0
 
-	if TP + FN == 0:
-		recall = 0.0
-	else:
-		recall = TP / (TP+FN)
+    for i in range(len(y_true)):
+        if y_true[i] == 1 and y_pred[i] == 1:
+            TP += 1
+        elif y_true[i] == 0 and y_pred[i] == 1:
+            FP += 1
+        elif y_true[i] == 1 and y_pred[i] == 0:
+            FN += 1
 
-	if precision == 0.0 and recall == 0.0:
-		return 0.0
-	else:
-		f = (1 + beta**2) * ((precision * recall) / ((precision * beta**2) + recall))
-		return round(f, 3)
-		
+    if TP + FP == 0:
+        precision = 0.0
+    else:
+        precision = TP / (TP + FP)
+
+    if TP + FN == 0:
+        recall = 0.0
+    else:
+        recall = TP / (TP + FN)
+
+    if precision == 0.0 and recall == 0.0:
+        return 0.0
+
+    f = (1 + beta**2) * (precision * recall) / (beta**2 * precision + recall)
+
+    return round(f, 3)
